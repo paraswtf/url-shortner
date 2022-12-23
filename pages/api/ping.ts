c61927cc3fe721dd95798fff2cbf9a5fbf8c8236
Paras Deshpande <parasbpndeshpande@gmail.com>
2022-12-23 10:04:21 +0530

    .

--- pages/api/ping.ts
+++
@@ -1,6 +0,0 @@
-import type { NextApiRequest, NextApiResponse } from "next";
-import { ApiPingErrorResponse, ApiPingResponse } from "../../lib/api";
-
-export default async function handler(req: NextApiRequest, res: NextApiResponse<ApiPingResponse | ApiPingErrorResponse>) {
-	res.status(200).json({ success: true, status: 200, message: "Pong!" });
-}
